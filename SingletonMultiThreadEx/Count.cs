733a2e4208a7142811ec306138401a22d28f03fe
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-24 22:47:30 +0200

    Add project files.

---
+++ SingletonMultiThreadEx/Count.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonMultiThreadEx
+{
+    internal class Count
+    {
+        private static Count instance= null;
+        private static Object locker = new object();
+        public  int Couunt=0;
+        private Count()
+        {   
+        }
+        public static Count getInstance()
+        {
+            if(instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Count();
+                    }
+                }
+            }
+            return instance;
+        }
+        public void increment ()
+        {
+            Couunt++;
+        }
+    }
+}
---
+++ SingletonMultiThreadEx/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonMultiThreadEx;
+Task task1 = Task.Factory.StartNew(() =>
+{
+    Count c1 = Count.getInstance();
+    c1.increment();
+    Console.WriteLine(" counter num 1 : " + c1.Couunt);
+});
+Task task2= Task.Factory.StartNew(() =>
+{
+    Count c2 = Count.getInstance();
+    c2.increment();
+    Console.WriteLine(" counter num 2 : " + c2.Couunt);
+});
+Console.ReadKey();
+
